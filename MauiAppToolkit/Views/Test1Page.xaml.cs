dbb0689e39af6ab0bf569cf822115310287a61e6
mabyre <mabyre@sodevlog.com>
2023-05-16 16:31:21 +0200

    Add Page

---
+++ MauiAppToolkit/Views/Test1Page.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Maui.Controls.Xaml;
+
+namespace MauiAppToolkit.Views;
+
+public partial class Test1Page : ContentPage
+{
+    public Test1Page()
+    {
+        InitializeComponent();
+    }
+}
