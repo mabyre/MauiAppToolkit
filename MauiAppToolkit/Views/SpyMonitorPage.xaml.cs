bf11ace3612b50904bd5c6408a66bdd883c885f2
mabyre <mabyre@sodevlog.com>
2023-06-12 10:38:45 +0200

    create SpyMonitor to get status of web cam

---
+++ MauiAppToolkit/Views/SpyMonitorPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Maui.Controls.Xaml;
+
+namespace MauiAppToolkit.Views;
+
+public partial class Page1 : ContentPage
+{
+    public Page1()
+    {
+        InitializeComponent();
+    }
+}
